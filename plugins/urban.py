724aa244048ecf8c334be35c32a61410d00c752f
michielaarts <michieljm@gmail.com>
2021-02-24 14:48:28 +0100

    Patch crossing conflicts

--- plugins/urban.py
+++ plugins/urban.py
@@ -37,19 +37,21 @@ def init_plugin():
     # Create dummy aircrafts
     stack.stack('CRE UAV001 M600 CG0002 90 100 15')
     stack.stack('CRE UAV002 M600 CG0000 90 100 30')
+    stack.stack('CRE UAV003 M600 CG0202 180 100 29')
     stack.stack('HOLD')
-    stack.stack('ADDWPT UAV001 CG0004')
-    stack.stack('ADDWPT UAV001 CG0404')
+    stack.stack('++')
+    stack.stack('ADDWPT UAV001 CG0004 100 30')
+    stack.stack('ADDWPT UAV001 CG0404 100 30')
     stack.stack('UAV001 DEST CG0409')
+    stack.stack('ADDWPT UAV003 CG0002 100 29')
+    stack.stack('ADDWPT UAV003 CG0004 100 29')
     stack.stack('POS UAV001')
     stack.stack('PAN UAV001')
     stack.stack('ZOOM 9')
     stack.stack('TRAIL ON')
 
-
-
     # Use entity class to prevent errors
-    example = Example()
+    # example = Example()
 
     # Configuration parameters
     config = {
@@ -194,7 +196,7 @@ class SpeedBased(ConflictResolution):
                 new_dv_magnitude = np.linalg.norm(dv_v)
 
                 if new_dv_magnitude > current_dv_magnitude:
-                    dv[idx] = dv_v
+                    dv[idx] += dv_v
 
         # Resolution vector for all aircraft, cartesian coordinates
         dv = np.transpose(dv)
@@ -287,65 +289,63 @@ class SpeedBased(ConflictResolution):
             # Calculate cosine angle.
             cos_angle = np.arccos(np.dot(v2, vrel) / (mag_v2 * mag_vrel))
 
-            # Determine time to incur for resolution
-            tr = (2 * s_h - mag_dcpa) / (mag_v2 * np.sin(cos_angle))
+            # Determine time to incur for resolution.
+            tr = (s_h - mag_dcpa) / (mag_v2 * np.sin(cos_angle))
 
-            # Simple version, slow down until resolved
-            return v1 * 0., idx1
+            vreso = v1 * tcpa / (tr + tcpa)
+            return vreso - v1, idx1
         else:
             raise ValueError(f'Something went wrong in speed_based(),\n' +
                              f'cpa_angle={cpa_angle},\n' +
                              f'app_angle={approach_angle}')
 
 
-
-
-
-### Entities in BlueSky are objects that are created only once (called singleton)
-### which implement some traffic or other simulation functionality.
-### To define an entity that ADDS functionality to BlueSky, create a class that
-### inherits from bluesky.core.Entity.
-### To replace existing functionality in BlueSky, inherit from the class that
-### provides the original implementation (see for example the asas/eby plugin).
-class Example(Entity):
-    """ Example new entity object for BlueSky. """
-
-    def __init__(self):
-        super().__init__()
-        # All classes deriving from Entity can register lists and numpy arrays
-        # that hold per-aircraft data. This way, their size is automatically
-        # updated when aircraft are created or deleted in the simulation.
-        with self.settrafarrays():
-            self.npassengers = np.array([])
-
-    def create(self, n=1):
-        """ This function gets called automatically when new aircraft are created. """
-        # Don't forget to call the base class create when you reimplement this function!
-        super().create(n)
-        # After base creation we can change the values in our own states for the new aircraft
-        self.npassengers[-n:] = [randint(0, 150) for _ in range(n)]
-
-    # Functions that need to be called periodically can be indicated to BlueSky
-    # with the timed_function decorator
-    @core.timed_function(name='example', dt=5)
-    def update(self):
-        """ Periodic update function for our example entity. """
-        return None
-        # stack.stack('ECHO Example update: creating a random aircraft')
-        # stack.stack('MCRE 1')
-
-    # You can create new stack commands with the stack.command decorator.
-    # By default, the stack command name is set to the function name.
-    # The default argument type is a case-sensitive word. You can indicate different
-    # types using argument annotations. This is done in the below function:
-    # - The acid argument is a BlueSky-specific argument with type 'acid'.
-    #       This converts callsign to the corresponding index in the traffic arrays.
-    # - The count argument is a regular int.
-    @stack.command
-    def passengers(self, acid: 'acid', count: int = -1):
-        """ Set the number of passengers on aircraft 'acid' to 'count'. """
-        if count < 0:
-            return True, f'Aircraft {traf.id[acid]} currently has {self.npassengers[acid]} passengers on board.'
-
-        self.npassengers[acid] = count
-        return True, f'The number of passengers on board {traf.id[acid]} is set to {count}.'
+#
+# ### Entities in BlueSky are objects that are created only once (called singleton)
+# ### which implement some traffic or other simulation functionality.
+# ### To define an entity that ADDS functionality to BlueSky, create a class that
+# ### inherits from bluesky.core.Entity.
+# ### To replace existing functionality in BlueSky, inherit from the class that
+# ### provides the original implementation (see for example the asas/eby plugin).
+# class Example(Entity):
+#     """ Example new entity object for BlueSky. """
+#
+#     def __init__(self):
+#         super().__init__()
+#         # All classes deriving from Entity can register lists and numpy arrays
+#         # that hold per-aircraft data. This way, their size is automatically
+#         # updated when aircraft are created or deleted in the simulation.
+#         with self.settrafarrays():
+#             self.npassengers = np.array([])
+#
+#     def create(self, n=1):
+#         """ This function gets called automatically when new aircraft are created. """
+#         # Don't forget to call the base class create when you reimplement this function!
+#         super().create(n)
+#         # After base creation we can change the values in our own states for the new aircraft
+#         self.npassengers[-n:] = [randint(0, 150) for _ in range(n)]
+#
+#     # Functions that need to be called periodically can be indicated to BlueSky
+#     # with the timed_function decorator
+#     @core.timed_function(name='example', dt=5)
+#     def update(self):
+#         """ Periodic update function for our example entity. """
+#         return None
+#         # stack.stack('ECHO Example update: creating a random aircraft')
+#         # stack.stack('MCRE 1')
+#
+#     # You can create new stack commands with the stack.command decorator.
+#     # By default, the stack command name is set to the function name.
+#     # The default argument type is a case-sensitive word. You can indicate different
+#     # types using argument annotations. This is done in the below function:
+#     # - The acid argument is a BlueSky-specific argument with type 'acid'.
+#     #       This converts callsign to the corresponding index in the traffic arrays.
+#     # - The count argument is a regular int.
+#     @stack.command
+#     def passengers(self, acid: 'acid', count: int = -1):
+#         """ Set the number of passengers on aircraft 'acid' to 'count'. """
+#         if count < 0:
+#             return True, f'Aircraft {traf.id[acid]} currently has {self.npassengers[acid]} passengers on board.'
+#
+#         self.npassengers[acid] = count
+#         return True, f'The number of passengers on board {traf.id[acid]} is set to {count}.'
