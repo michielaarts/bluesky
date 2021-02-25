13bf78125683df8607b497b87345d8710c83c365
michielaarts <michieljm@gmail.com>
2021-02-25 14:06:31 +0100

    Update speed_based()
    Patched in-airway coming from behind conflicts

--- plugins/urban.py
+++ plugins/urban.py
@@ -33,13 +33,11 @@ def init_plugin():
 
     # MVP Speed based accelerates instead of decelerating..
 
-
     # Create dummy aircrafts
     stack.stack('CRE UAV001 M600 CG0002 90 100 15')
     stack.stack('CRE UAV002 M600 CG0000 90 100 30')
     stack.stack('CRE UAV003 M600 CG0202 180 100 29')
     stack.stack('HOLD')
-    stack.stack('++')
     stack.stack('ADDWPT UAV001 CG0004 100 30')
     stack.stack('ADDWPT UAV001 CG0404 100 30')
     stack.stack('UAV001 DEST CG0409')
@@ -47,7 +45,7 @@ def init_plugin():
     stack.stack('ADDWPT UAV003 CG0004 100 29')
     stack.stack('POS UAV001')
     stack.stack('PAN UAV001')
-    stack.stack('ZOOM 9')
+    stack.stack('ZOOM 20')
     stack.stack('TRAIL ON')
 
     # Use entity class to prevent errors
@@ -88,93 +86,6 @@ def load_city_grid(n_rows: int, n_cols: int, row_sep: float, col_sep: float):
 class SpeedBased(ConflictResolution):
     def __init__(self):
         super().__init__()
-
-    def setprio(self, flag=None, priocode=''):
-        """Set the prio switch and the type of prio """
-        if flag is None:
-            return True, "PRIORULES [ON/OFF] [PRIOCODE]" + \
-                   "\nAvailable priority codes: " + \
-                   "\n     FF1:  Free Flight Primary (No Prio) " + \
-                   "\n     FF2:  Free Flight Secondary (Cruising has priority)" + \
-                   "\n     FF3:  Free Flight Tertiary (Climbing/descending has priority)" + \
-                   "\n     LAY1: Layers Primary (Cruising has priority + horizontal resolutions)" + \
-                   "\n     LAY2: Layers Secondary (Climbing/descending has priority + horizontal resolutions)" + \
-                   "\nPriority is currently " + ("ON" if self.swprio else "OFF") + \
-                   "\nPriority code is currently: " + \
-                   str(self.priocode)
-        options = ["FF1", "FF2", "FF3", "LAY1", "LAY2"]
-        if priocode not in options:
-            return False, "Priority code Not Understood. Available Options: " + str(options)
-        return super().setprio(flag, priocode)
-
-    def applyprio(self, dv_mvp, dv1, dv2, vs1, vs2):
-        """ Apply the desired priority setting to the resolution """
-
-        # Primary Free Flight prio rules (no priority)
-        if self.priocode == 'FF1':
-            # since cooperative, the vertical resolution component can be halved, and then dv_mvp can be added
-            dv_mvp[2] = dv_mvp[2] / 2.0
-            dv1 = dv1 - dv_mvp
-            dv2 = dv2 + dv_mvp
-
-        # Secondary Free Flight (Cruising aircraft has priority, combined resolutions)
-        if self.priocode == 'FF2':
-            # since cooperative, the vertical resolution component can be halved, and then dv_mvp can be added
-            dv_mvp[2] = dv_mvp[2] / 2.0
-            # If aircraft 1 is cruising, and aircraft 2 is climbing/descending -> aircraft 2 solves conflict
-            if abs(vs1) < 0.1 and abs(vs2) > 0.1:
-                dv2 = dv2 + dv_mvp
-            # If aircraft 2 is cruising, and aircraft 1 is climbing -> aircraft 1 solves conflict
-            elif abs(vs2) < 0.1 and abs(vs1) > 0.1:
-                dv1 = dv1 - dv_mvp
-            else:  # both are climbing/descending/cruising -> both aircraft solves the conflict
-                dv1 = dv1 - dv_mvp
-                dv2 = dv2 + dv_mvp
-
-        # Tertiary Free Flight (Climbing/descending aircraft have priority and crusing solves with horizontal
-        # resolutions)
-        elif self.priocode == 'FF3':
-            # If aircraft 1 is cruising, and aircraft 2 is climbing/descending -> aircraft 1 solves conflict
-            # horizontally
-            if abs(vs1) < 0.1 and abs(vs2) > 0.1:
-                dv_mvp[2] = 0.0
-                dv1 = dv1 - dv_mvp
-            # If aircraft 2 is cruising, and aircraft 1 is climbing -> aircraft 2 solves conflict horizontally
-            elif abs(vs2) < 0.1 and abs(vs1) > 0.1:
-                dv_mvp[2] = 0.0
-                dv2 = dv2 + dv_mvp
-            else:  # both are climbing/descending/cruising -> both aircraft solves the conflict, combined
-                dv_mvp[2] = dv_mvp[2] / 2.0
-                dv1 = dv1 - dv_mvp
-                dv2 = dv2 + dv_mvp
-
-        # Primary Layers (Cruising aircraft has priority and clmibing/descending solves. All conflicts solved horizontally)
-        elif self.priocode == 'LAY1':
-            dv_mvp[2] = 0.0
-            # If aircraft 1 is cruising, and aircraft 2 is climbing/descending -> aircraft 2 solves conflict horizontally
-            if abs(vs1) < 0.1 and abs(vs2) > 0.1:
-                dv2 = dv2 + dv_mvp
-            # If aircraft 2 is cruising, and aircraft 1 is climbing -> aircraft 1 solves conflict horizontally
-            elif abs(vs2) < 0.1 and abs(vs1) > 0.1:
-                dv1 = dv1 - dv_mvp
-            else:  # both are climbing/descending/cruising -> both aircraft solves the conflict horizontally
-                dv1 = dv1 - dv_mvp
-                dv2 = dv2 + dv_mvp
-
-        # Secondary Layers (Climbing/descending aircraft has priority and cruising solves. All conflicts solved horizontally)
-        elif self.priocode == 'LAY2':
-            dv_mvp[2] = 0.0
-            # If aircraft 1 is cruising, and aircraft 2 is climbing/descending -> aircraft 1 solves conflict horizontally
-            if abs(vs1) < 0.1 and abs(vs2) > 0.1:
-                dv1 = dv1 - dv_mvp
-            # If aircraft 2 is cruising, and aircraft 1 is climbing -> aircraft 2 solves conflict horizontally
-            elif abs(vs2) < 0.1 and abs(vs1) > 0.1:
-                dv2 = dv2 + dv_mvp
-            else:  # both are climbing/descending/cruising -> both aircraft solves the conflic horizontally
-                dv1 = dv1 - dv_mvp
-                dv2 = dv2 + dv_mvp
-
-        return dv1, dv2
 
     def resolve(self, conf, ownship, intruder):
         """ Resolve all current conflicts """
@@ -220,10 +131,13 @@ class SpeedBased(ConflictResolution):
 
         return newtrack, newgscapped, vscapped, ownship.selalt
 
-
     def speed_based(self, ownship, intruder, conf, qdr, dist, tcpa, tLOS, idx1, idx2):
         """Constrained speed-based resolution algorithm"""
         # Preliminary calculations
+
+        if tcpa < 0:
+            # Closest point already passed. Do nothing.
+            return np.zeros(3), idx1
 
         if ownship != intruder:
             raise ValueError('ownship != intruder ??\n', ownship.tas, intruder.tas)
@@ -257,25 +171,40 @@ class SpeedBased(ConflictResolution):
         dcpa_angle = np.arctan2(dcpa[0], dcpa[1])
         cpa_angle = (np.deg2rad(ownship.trk[idx1]) - dcpa_angle) % (2 * np.pi)
 
-        # Original detection distance (to use as factor).
-        mag_original_vrel = abs(intruder.ap.tas[idx2] - ownship.ap.tas[idx1])
-        original_detection_distance = mag_original_vrel * conf.dtlookahead + s_h
-
         # Check if flying in same direction
         behind_angle = np.deg2rad(10)
         if approach_angle < behind_angle or approach_angle > 2 * np.pi - behind_angle:
             # Ownship coming from behind, needs to decelerate to intruder speed.
             # Within distance drel - 2S_h, as approaching still beneficial.
 
+            # Time to LoS
+            t_s_h = s_h / mag_v2
+            time_to_los = tcpa - t_s_h
+            if time_to_los < 0:
+                raise ValueError('Loss of separation!')
+
+            # a * 0 + b = v1
+            # a * time_to_los + b = v2
+            # scale_factor = - mag_vrel / time_to_los
+            # decelerate_dv = scale_factor * settings.asas_dt
+            # decelerate_factor = decelerate_dv / mag_v1
+
+            original_detection_dist = conf.dtlookahead * ownship.ap.tas[idx1]
+
+            # a * original_detection_dist + b = ownship.ap.tas
+            # a * s_h + b = v2
+            scale_factor = (ownship.ap.tas[idx1] - mag_v2) / (original_detection_dist - s_h)
+            correction_factor = mag_v2 - scale_factor * s_h
+            resolution_spd = scale_factor * dist + correction_factor
+            decelerate_factor = 1 - resolution_spd / mag_v1
+
+            ownship_acid = ownship.id[idx1]
+            intruder_acid = intruder.id[idx2]
             # Decelerate factor.
-            # original_detection_distance * a + b = 0.
-            # 2*s_h * a + b = 1.
-            scale_factor = -1 / (original_detection_distance - s_h)
-            correction = - original_detection_distance * scale_factor
-            decelerate_factor = scale_factor * dist + correction
-            if decelerate_factor < 0:
-                raise ValueError(f'Something went wrong in speed_based(), decelerate_factor={decelerate_factor}')
-            return vrel * decelerate_factor, idx1
+            if resolution_spd > ownship.ap.tas[idx1]:
+                raise ValueError(f'Something went wrong in speed_based(), accelerating {ownship.id[idx1]}!\n' +
+                                 f'resolution_spd={resolution_spd}')
+            return -v1 * decelerate_factor, idx1
         elif np.pi - behind_angle < approach_angle < np.pi + behind_angle:
             # Intruder must resolve conflict from behind.
             # Do nothing.
@@ -298,54 +227,3 @@ class SpeedBased(ConflictResolution):
             raise ValueError(f'Something went wrong in speed_based(),\n' +
                              f'cpa_angle={cpa_angle},\n' +
                              f'app_angle={approach_angle}')
-
-
-#
-# ### Entities in BlueSky are objects that are created only once (called singleton)
-# ### which implement some traffic or other simulation functionality.
-# ### To define an entity that ADDS functionality to BlueSky, create a class that
-# ### inherits from bluesky.core.Entity.
-# ### To replace existing functionality in BlueSky, inherit from the class that
-# ### provides the original implementation (see for example the asas/eby plugin).
-# class Example(Entity):
-#     """ Example new entity object for BlueSky. """
-#
-#     def __init__(self):
-#         super().__init__()
-#         # All classes deriving from Entity can register lists and numpy arrays
-#         # that hold per-aircraft data. This way, their size is automatically
-#         # updated when aircraft are created or deleted in the simulation.
-#         with self.settrafarrays():
-#             self.npassengers = np.array([])
-#
-#     def create(self, n=1):
-#         """ This function gets called automatically when new aircraft are created. """
-#         # Don't forget to call the base class create when you reimplement this function!
-#         super().create(n)
-#         # After base creation we can change the values in our own states for the new aircraft
-#         self.npassengers[-n:] = [randint(0, 150) for _ in range(n)]
-#
-#     # Functions that need to be called periodically can be indicated to BlueSky
-#     # with the timed_function decorator
-#     @core.timed_function(name='example', dt=5)
-#     def update(self):
-#         """ Periodic update function for our example entity. """
-#         return None
-#         # stack.stack('ECHO Example update: creating a random aircraft')
-#         # stack.stack('MCRE 1')
-#
-#     # You can create new stack commands with the stack.command decorator.
-#     # By default, the stack command name is set to the function name.
-#     # The default argument type is a case-sensitive word. You can indicate different
-#     # types using argument annotations. This is done in the below function:
-#     # - The acid argument is a BlueSky-specific argument with type 'acid'.
-#     #       This converts callsign to the corresponding index in the traffic arrays.
-#     # - The count argument is a regular int.
-#     @stack.command
-#     def passengers(self, acid: 'acid', count: int = -1):
-#         """ Set the number of passengers on aircraft 'acid' to 'count'. """
-#         if count < 0:
-#             return True, f'Aircraft {traf.id[acid]} currently has {self.npassengers[acid]} passengers on board.'
-#
-#         self.npassengers[acid] = count
-#         return True, f'The number of passengers on board {traf.id[acid]} is set to {count}.'
