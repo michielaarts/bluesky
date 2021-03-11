cacb0152f2c103d820947510234fc5fc250f5031
michielaarts <michieljm@gmail.com>
2021-03-11 16:46:44 +0100

    Prevent instant LoS at creation

--- utils/scenario_generator.py
+++ utils/scenario_generator.py
@@ -36,7 +36,8 @@ class ScenarioGenerator:
         self.avg_route_length = self.urban_grid.avg_route_length * 1000  # m
 
     def create_scenario(self, n_inst: np.ndarray, speed: np.ndarray, duration: np.ndarray,
-                        h_sep: np.ndarray, t_lookahead: np.ndarray, ac_type: str = 'M600') -> list:
+                        h_sep: np.ndarray, v_sep: np.ndarray, t_lookahead: np.ndarray, ac_type: str = 'M600'
+                        ) -> list:
         """
         Creates a scenario for each value of n_inst, or for a single n_inst.
         All other inputs are either an array of len(n_inst) or a float.
@@ -45,6 +46,7 @@ class ScenarioGenerator:
         :param speed: Undisturbed speed of aircraft [m/s] - optionally an array
         :param duration: Scenario duration [s] - optionally an array
         :param h_sep: horizontal separation [m] - optionally an array
+        :param v_sep: vertical separation [ft] - optionally an array
         :param t_lookahead: look-ahead time [s] - optionally an array
         :param ac_type: Type of aircraft
         """
@@ -57,6 +59,8 @@ class ScenarioGenerator:
             duration = np.array([duration] * len(n_inst))
         if isinstance(h_sep, float):
             h_sep = np.array([h_sep] * len(n_inst))
+        if isinstance(v_sep, float):
+            v_sep = np.array([v_sep] * len(n_inst))
         if isinstance(t_lookahead, float):
             t_lookahead = np.array([t_lookahead] * len(n_inst))
 
@@ -65,14 +69,16 @@ class ScenarioGenerator:
         if len(duration) != len(n_inst):
             raise ValueError('Length of duration array does not match density array')
         if len(h_sep) != len(n_inst):
-            raise ValueError('Length of separation array does not match density array')
+            raise ValueError('Length of horizontal separation array does not match density array')
+        if len(v_sep) != len(n_inst):
+            raise ValueError('Length of vertical separation array does not match density array')
         if len(t_lookahead) != len(n_inst):
             raise ValueError('Length of look-ahead time array does not match density array')
 
         # Loop through densities.
         all_scen = []
-        for (n_i, V, T, s_h, t_l) in zip(n_inst, speed, duration, h_sep, t_lookahead):
-            print(n_i, V, T, s_h, t_l)
+        for (n_i, V, T, s_h, s_v, t_l) in zip(n_inst, speed, duration, h_sep, v_sep, t_lookahead):
+            print(n_i, V, T, s_h, s_v, t_l)
 
             # Determine departure times.
             avg_route_duration = self.avg_route_length / V
@@ -106,7 +112,7 @@ class ScenarioGenerator:
 
             scen_dict = {'n_inst': n_i, 'n_total': n_total,
                          'speed': V, 'duration': T,
-                         's_h': s_h, 't_l': t_l,
+                         's_h': s_h, 's_v': s_v, 't_l': t_l,
                          'scenario': all_ac}
             all_scen.append(scen_dict)
         return all_scen
@@ -130,7 +136,6 @@ class ScenarioGenerator:
         :param filepath: path to scenario folder
         :return: None
         """
-        alt = 0  # For now, maybe implement later
 
         if not filepath.is_dir():
             filepath.mkdir(parents=True, exist_ok=True)
@@ -138,7 +143,12 @@ class ScenarioGenerator:
         for scen in all_scen:
             n_inst = scen['n_inst']
             spd = scen['speed']
+            tas = spd / kts
             duration = scen['duration']
+
+            # Let aircraft climb slightly to cruise altitude, to prevent LoS at creation.
+            cruise_alt = 50.  # ft
+            departure_alt = cruise_alt - scen['s_v'] * 1.1  # ft
 
             filename = f'{prefix}_N{n_inst:.0f}_V{spd:.0f}_T{duration:.0f}.scn'
             with open(filepath / filename, 'w') as f:
@@ -148,6 +158,7 @@ class ScenarioGenerator:
                 f.write(f'# Speed: {spd:.1f}m/s\n')
                 f.write(f'# Duration: {duration:.1f}s\n')
                 f.write(f'# Horizontal separation: {scen["s_h"]:.1f}m\n')
+                f.write(f'# Vertical separation: {scen["s_v"]:.1f}ft\n')
                 f.write(f'# Look-ahead time: {scen["t_l"]:.1f}s\n')
                 f.write(f'# Mean route length: {self.avg_route_length:.1f}m\n')
                 f.write('# ########################################### #\n')
@@ -159,6 +170,7 @@ class ScenarioGenerator:
                 f.write(f'{self.tim2txt(0)}>ASAS {asas}\n')
                 f.write(f'{self.tim2txt(0)}>RESO {reso}\n')
                 f.write(f'{self.tim2txt(0)}>ZONER {scen["s_h"] / nm}\n')
+                f.write(f'{self.tim2txt(0)}>ZONEDH {scen["s_v"] / 2}\n')
                 f.write(f'{self.tim2txt(0)}>DTLOOK {scen["t_l"]}\n\n')
 
                 # Pan screen to center node, zoom and hold simulation
@@ -184,11 +196,12 @@ class ScenarioGenerator:
 
                     # Write to .scn file.
                     f.write(f'\n# Creating aircraft no. {ac["id"]}\n')
-                    f.write(f'{time_string}>CRE {callsign} {ac["ac_type"]} {origin} {hdg} {alt} {spd / kts}\n')
+                    f.write(f'{time_string}>CRE {callsign} {ac["ac_type"]} '
+                            f'{origin} {hdg} {departure_alt} {tas}\n')
                     f.write(f'{time_string}>ORIG {callsign} {origin}\n')
                     f.write(f'{time_string}>DEST {callsign} {ac["destination"]}\n')
                     for wpt in ac["path"][1:-1]:
-                        f.write(f'{time_string}>ADDWPT {callsign} {wpt}\n')
+                        f.write(f'{time_string}>ADDWPT {callsign} {wpt} {cruise_alt} {tas}\n')
                     f.write(f'{time_string}>LNAV {callsign} ON\n')
                     f.write(f'{time_string}>VNAV {callsign} ON\n')
                 print(f'Written {filepath / filename}')
@@ -204,8 +217,9 @@ if __name__ == '__main__':
     HORIZONTAL_SEPARATION_KM = 0.2  # km
     VERTICAL_SEPARATION_KM = 0.2  # km
     S_H = 50.  # m
+    S_V = 25.  # ft
     T_L = 10.  # s
 
     scen_gen = ScenarioGenerator(N_ROWS, N_COLS, HORIZONTAL_SEPARATION_KM, VERTICAL_SEPARATION_KM)
-    all_scenarios = scen_gen.create_scenario(N_INST, SPEED, DURATION, S_H, T_L)
+    all_scenarios = scen_gen.create_scenario(N_INST, SPEED, DURATION, S_H, S_V, T_L)
     scen_gen.write_scenario(all_scenarios, reso='off')
