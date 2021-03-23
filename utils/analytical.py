43d35c1ec510ebfcc9b97747ffff7e8b7e5ac3cc
michielaarts <michieljm@gmail.com>
2021-03-23 16:10:54 +0100

    Delay model v1

--- utils/analytical.py
+++ utils/analytical.py
@@ -32,6 +32,7 @@ class AnalyticalModel:
 
         self.cruise_alt = 50.
         self.departure_alt = self.cruise_alt - self.s_v * 1.5
+        self.t_X = self.s_h / self.speed  # Time to cross an intersection [s]
 
         # Sanity check.
         if self.urban_grid.grid_height != self.urban_grid.grid_width or \
@@ -46,7 +47,8 @@ class AnalyticalModel:
         self.area = np.power(self.urban_grid.grid_height * 1000 * (self.urban_grid.n_rows - 1), 2)
         self.c_inst_nr = self.nr_model()
 
-        self.delay_model()
+        self.delays = self.delay_model(self.flow_rates)
+
         self.wr_model()
 
     def nr_model(self) -> np.ndarray:
@@ -55,13 +57,13 @@ class AnalyticalModel:
         c_inst_nr_crossing = 4 * np.power(self.n_inst / 4, 2) * 2 * self.s_h * vrel * self.t_l / self.area
 
         # Self interaction with departing traffic. Same as crossing, but in xz-plane.
-        vrel = self.vs
         alt_to_climb = self.cruise_alt - self.departure_alt
         time_to_climb = alt_to_climb / self.vs
         n_inst_departing = self.departure_rate * time_to_climb
         n_inst_cruise = self.n_inst - n_inst_departing
         area = alt_to_climb * np.sqrt(self.area)
-        c_inst_nr_departing = n_inst_departing * n_inst_cruise * 2 * self.s_v * vrel * self.t_l / area
+        c_inst_nr_departing = n_inst_departing * n_inst_cruise * 2 * self.s_v * self.vs * self.t_l / area
+
         return c_inst_nr_crossing + c_inst_nr_departing
 
     def determine_flow_rates(self) -> pd.DataFrame:
@@ -76,17 +78,87 @@ class AnalyticalModel:
         spawn_rate = self.n_inst / avg_route_duration
         return spawn_rate
 
-    @staticmethod
-    def general_delay():
+    def general_delay(self):
         pass
 
     @staticmethod
     def stochastic_delay():
         pass
 
-    def delay_model(self) -> None:
-        for ni in self.n_inst:
-            pass
+    def delay_model(self, flow_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Calculates the delay per vehicle at each node for the provided flow dataframe.
+
+        :param flow_df:
+        :return: Delay dataframe
+        """
+        isct_delays = pd.DataFrame(columns=self.urban_grid.isct_nodes, index=self.n_inst)
+        departure_delays = pd.DataFrame(columns=self.urban_grid.od_nodes, index=self.n_inst)
+
+        # Intersection delays.
+        # Group by direction per intersection.
+        from_flows = flow_df.groupby(['from', 'via']).sum()
+        for isct in self.urban_grid.isct_nodes:
+            isct_flows = from_flows.iloc[
+                from_flows.index.get_level_values('via') == isct
+            ].copy()
+            from_nodes = isct_flows.index.get_level_values('from').unique()
+            if len(from_nodes) == 1:
+                # Border node, no intersection. Skip.
+                continue
+            elif len(from_nodes) != 2:
+                # Sanity check.
+                raise ValueError(f'Intersection with {len(from_nodes)} directions found!\n', isct_flows)
+
+            total_q = isct_flows.sum()
+            total_y = total_q * self.t_X
+            total_stochastic_delay = total_y * total_y / (2 * total_q * (1 - total_y))
+            isct_delay = total_q.copy() * 0
+            flow_1 = isct_flows.iloc[0].squeeze()
+            flow_2 = isct_flows.iloc[1].squeeze()
+            for (q_g, q_r) in [(flow_1, flow_2),
+                               (flow_2, flow_1)]:
+                # General delay.
+                lambda_u = 1 - self.t_X * q_r
+                c_u = 1 / q_r
+                y = q_g * self.t_X
+                general_delay = c_u * np.power(1 - lambda_u, 2) / (2 * (1 - y))
+
+                # Stochastic delay.
+                stochastic_flow_delay = y * y / (2 * q_g * (1 - y))
+                stochastic_delay = total_stochastic_delay - stochastic_flow_delay
+
+                isct_delay = isct_delay + general_delay + stochastic_delay
+            isct_delays[isct] = isct_delay
+
+        # Departure delays.
+        via_flows = flow_df.groupby('via').sum()
+        departure_flow_per_node = self.departure_rate / len(self.urban_grid.od_nodes)
+        for origin in self.urban_grid.od_nodes:
+            passing_flow = via_flows.iloc[via_flows.index == origin].squeeze()
+
+            total_q = passing_flow + departure_flow_per_node
+            total_y = total_q * self.t_X
+            total_stochastic_delay = total_y * total_y / (2 * total_q * (1 - total_y))
+            departure_delay = total_q.copy() * 0
+            for (q_g, q_r) in [(passing_flow, departure_flow_per_node),
+                               (departure_flow_per_node, passing_flow)]:
+                # General delay.
+                lambda_u = 1 - self.t_X * q_r
+                c_u = 1 / q_r
+                y = q_g * self.t_X
+                general_delay = c_u * np.power(1 - lambda_u, 2) / (2 * (1 - y))
+
+                # Stochastic delay.
+                stochastic_flow_delay = y * y / (2 * q_g * (1 - y))
+                stochastic_delay = total_stochastic_delay - stochastic_flow_delay
+
+                departure_delay = departure_delay + general_delay + stochastic_delay
+            departure_delays[origin] = departure_delay
+
+        # Merge delays.
+        delays = isct_delays.T.append(departure_delays.T)
+        return delays
 
     def wr_model(self) -> None:
         pass
